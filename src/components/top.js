586a47e88e194ea63739daf74be505c99381b4a1
waihoyu <13701343809@qq.com>
2019-08-30 10:10:14 +0800

    third time

---
+++ src/components/top.js
@@ -0,0 +1,11 @@
+import Taro ,{Component} from '@tarojs/taro'
+import {View, Text, Image} from '@tarojs/components'
+
+
+class Top extends Component{
+    render(){
+        return (<View>头部操作栏</View>)
+    }
+}
+
+export default Top;
