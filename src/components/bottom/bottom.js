8a2f663c2b5c8821f122f1f95560f41992b1c195
waihoyu <13701343809@qq.com>
2019-09-13 11:57:12 +0800

    增加购物车

--- src/components/bottom/bottom.js
+++ src/components/bottom/bottom.js
@@ -14,10 +14,27 @@ import './bottom.less'
 class Bottom  extends Component {                
    constructor(){                                    
        super(...arguments)                           
-       this.state = { }                              
+       this.state = { 
+           Num:10,
+           sendPrice:3,
+           supportTakeBySelf:false,
+           sendMustPrice:20
+       }                              
    }                                                   
-   render(){                                         
-       return (<View>Bottom</View>)                
+   render(){     
+       let {Num, sendPrice, supportTakeBySelf, sendMustPrice} = this.state;                                    
+       return (<View className="bottom">
+           <View className="bottom_body">
+                {Num? <Text className="num">{Num}</Text>:null}
+                <Image className="store_img" src={require('../../assets/img/emptystore.png')}>
+                </Image>
+                <View className="info">
+                    <Text >{sendPrice? "另需配送费￥ "+ sendPrice:""}</Text>
+                    <Text>{supportTakeBySelf? "支持自取"+ sendPrice:"不支持自取"}</Text>
+                </View>
+                <View className="submit"><Text>{sendMustPrice?"￥"+ sendMustPrice+ "起送":"'"}</Text></View>
+           </View>
+       </View>)                
    }                                                 
  }
  export default Bottom                             
